99375b0c7b2ebf5d752ee04001b716d098430759
Tim <tim21615@gmail.com>
2019-07-26 12:30:36 +0800

    upload

--- superlihi_landing/js/all.js
+++ superlihi_landing/js/all.js
@@ -9,8 +9,26 @@ window.onunload = function(){
     imgTarget.css('height',titleTarget.height()-12);
 }
 
+
+if($(window).width()<470 && $("#br_tag").length ==0){
+    // var br_tag = document.createElement("br");
+    $("#imgTarget").after("<br id='br_tag' />");
+
+}
+
 $(document).ready(function(){
     $(window).resize(function() {
+
+        if($(window).width()<470 && $("#br_tag").length ==0){
+            // var br_tag = document.createElement("br");
+            $("#imgTarget").after("<br id='br_tag' />");
+
+        }
+
+        if($(window).width()>=470 && $("#br_tag").length !=0){
+            $("#br_tag").remove();
+        }
+
         imgTarget.css('height',titleTarget.height()-12);
     });
 });
